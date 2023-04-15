963b205b0900e26d16eaccef967787be5f17f698
muulinha <lucasfl.araujo@hotmail.com>
2023-04-16 05:43:55 +0800

    Created App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './components/pages/About';
+import Project from './components/pages/Project';
+import Contact from './components/pages/Contact';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className=''>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/about" render={<About />}/>
+            <Route path="/projects" render={<Project />}/>
+            <Route path="/contact" render={<Contact />}/>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
