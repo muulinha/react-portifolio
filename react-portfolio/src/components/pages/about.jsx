c4212af478cc144f7534d817b4c8ecd879884ef1
muulinha <lucasfl.araujo@hotmail.com>
2023-04-17 16:28:42 +0800

    Project link created

--- react-portfolio/src/components/pages/about.jsx
+++ react-portfolio/src/components/pages/about.jsx
@@ -5,13 +5,15 @@ function About() {
   return (
     <section id="aboutme" className="aboutme">
       <h1 className='page-header'>About Me</h1>
+      <div className='profile'>
       <img className="profilepic" src={Lucas} alt="lucas"/>
-      <p>
+      <p className='about-text'>
         Hi there, my name is Lucas Araujo. I am a full-stack web developer, holding a Bachelor’s degree in Mechanical Engineering. 
         I highly bring project management skills, leadership skills, communication skills and a lifetime working in a fast-paced high-pressure environments. 
-        I recently earned a certificate in full-stack development boot camp from the University of Sydney, 
+        <br></br><br></br>I recently earned a certificate in full-stack development boot camp from the University of Sydney, 
         where I developed skills in JavaScript, CSS, React.js, Node.js, responsive web design, and much more.
       </p>
+      </div>
     </section>
   );
 }
--- react-portfolio/src/components/pages/project.jsx
+++ react-portfolio/src/components/pages/project.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React from 'react';
+import ToDoList from '../../assets/todolist.png';
+import Weather from '../../assets/weather-dashboard.png';
+import Destination from '../../assets/destination.jpg';
+
+import '../../index.css';
+import './Project.css'
+
 
 function Project() {
-    return <div> this is the Project page </div>;
-  }
+  return (
+      <section className="portfolio">
+      <div className="center">
+          <h1 className="page-header">My Portfolio</h1>
+      </div>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4 portfolio">
+          <div class="col">
+              <div class="card">
+                  <img
+                      src={ToDoList}
+                      alt="ToDoList"
+                      class="card-img-top"
+                  />
+                  <div class="card-body">
+                      <p className="center">To Do Tracker</p>
+                      <a href="https://github.com/muulinha/to-do-list-react" target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">GitHub</button>
+                      </a>
+                      <a href="https://muulinha.github.io/to-do-list-react/" target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">Website</button>
+                      </a>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col">
+              <div class="card">
+                  <img
+                      src={Weather}
+                      alt="Weather"
+                      class="card-img-top"
+                  />
+                  <div class="card-body">
+                      <p className="center">Weather Dashboard</p>
+                      <a href="https://github.com/muulinha/Weather-Dashboard" target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">GitHub</button>
+                      </a>
+                      <a href="https://muulinha.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">Website</button>
+                      </a>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col">
+              <div class="card">
+                  <img
+                      src={Destination}
+                      alt="Destination"
+                      class="card-img-top"
+                  />
+                  <div class="card-body">
+                      <p className="center">Destination Inspiration</p>
+                      <a href="https://github.com/muulinha/Destination_Inspiration" target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">GitHub</button>
+                      </a>
+                      <a href="https://muulinha.github.io/Destination_Inspiration/" target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">Website</button>
+                      </a>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+  </section>
+  )
+}
   
   export default Project;
