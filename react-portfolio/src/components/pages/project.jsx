5c517de0216c872066c1eee70a51bd78d3e52934
muulinha <lucasfl.araujo@hotmail.com>
2023-04-17 15:15:46 +0800

    Fixed css and installed more node libraries

--- react-portfolio/src/components/pages/project.jsx
+++ react-portfolio/src/components/pages/project.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Project() {
-    return <div> this is the Project page</div>;
+    return <div> this is the Project page </div>;
   }
   
   export default Project;
